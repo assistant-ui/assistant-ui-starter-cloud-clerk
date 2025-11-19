e094d1aaea0d412ffffe08d880645018e39061ba
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2025-11-18 17:23:15 -0800

    update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,9 +1,7 @@
 import { openai } from "@ai-sdk/openai";
-// import { AnthropicProviderOptions } from "@ai-sdk/anthropic";
 import {
   streamText,
   convertToModelMessages,
-  // gateway,
   type UIMessage,
 } from "ai";
 
@@ -11,22 +9,15 @@ export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
 
   const result = streamText({
-    model: openai("gpt-5"),
+    model: openai.responses("gpt-5-nano"),
     messages: convertToModelMessages(messages),
     providerOptions: {
       openai: {
-        reasoningEffort: "medium",
+        reasoningEffort: "low",
         reasoningSummary: "auto",
       },
-      // anthropic: {
-      //   thinking: { type: 'enabled', budgetTokens: 12000 },
-      // } satisfies AnthropicProviderOptions,
     },
   });
-
-  // result.providerMetadata
-  //   .then((metadata) => console.log(JSON.stringify(metadata, null, 2)))
-  //   .catch((error) => console.error("Failed to fetch", error));
 
   return result.toUIMessageStreamResponse({
     sendReasoning: true,
